c6053e02731f64cc504df297fdb6269d7d0c5f01
flmezei <flmezei@gmail.com>
2022-08-16 14:32:09 -0300

    first commit

---
+++ database/migrations/2022_06_08_170857_alter_fornecedores_novas_colunas.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterFornecedoresNovasColunas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->string('uf', 2);
+            $table->string('email', 150);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('fornecedores', function (Blueprint $table) {
+            //para remover colunas
+            //$table->dropColumn('uf'); - modo individual
+            //$table->dropColumn('email'); - modo individual
+            $table->dropColumn(['uf', 'email']); //array com todas as colunas que deseja remover
+        });
+    }
+}
